b1474c200554a7a98c17b36c469352902bdaa827
Jesse Kopala <jdkopala@gmail.com>
2022-06-09 19:54:39 -0600

    Removed an extra conditional from the code. It looks nicer now.

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,18 +4,14 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
-      return("error: ", error);
+      return ("error: ", error);
     }
-    if (body === []) {
-      return "Invalid input, please make sure you input a cat breed"
+    let data = JSON.parse(body);
+    // console.log("data: ", data)
+    if (data[0] === undefined) {
+      callback("There was an invalid input, please try again");
     } else {
-      let data = JSON.parse(body);
-      // console.log("data: ", data)
-      if (data[0] === undefined) {
-        callback("There was an invalid input, please try again");
-      } else {
-        callback(error, data[0].description)
-      }
+      callback(null, data[0].description);
     }
   });
 };
